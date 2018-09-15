b14d8b6b4a3951f7bdc9c86b37e32e28f138766c
MrTj458 <mrtj458@gmail.com>
2018-09-15 13:51:18 -0600

    Fixed indentation

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -8,47 +8,47 @@ const styles = {
 const Card = ({ id, question, answer, show, updateCard, deleteCard }) => { 
   if(show) {
     return (
-    <div className="row">
-      <div className="col s12 m6">
-        <div className="card blue-grey darken-1">
-          <div className="card-content white-text">
-            <span className="card-title">Question</span>
-            <p>{question}</p>
-            <span className="card-title">Answer</span>
-            <p>{answer}</p>
-          </div>
-          <div className="card-action">
-            <div onClick={() => updateCard(id)} style={styles.margin} className="btn waves-effect">
-              Reveal Answer
+      <div className="row">
+        <div className="col s12 m6">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">Question</span>
+              <p>{question}</p>
+              <span className="card-title">Answer</span>
+              <p>{answer}</p>
             </div>
-            <div className="btn red waves-effect" onClick={() => deleteCard(id)}>
-              Delete
+            <div className="card-action">
+              <div onClick={() => updateCard(id)} style={styles.margin} className="btn waves-effect">
+                Reveal Answer
+              </div>
+              <div className="btn red waves-effect" onClick={() => deleteCard(id)}>
+                Delete
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     )
   } else {
     return (
-    <div className="row">
-      <div className="col s12 m6">
-        <div className="card blue-grey darken-1">
-          <div className="card-content white-text">
-            <span className="card-title">Question</span>
-            <p>{question}</p>
-          </div>
-          <div className="card-action">
-            <div onClick={() => updateCard(id)} style={styles.margin} className="btn waves-effect">
-              Reveal Answer
+      <div className="row">
+        <div className="col s12 m6">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">Question</span>
+              <p>{question}</p>
             </div>
-            <div className="btn red waves-effect" onClick={() => deleteCard(id)}>
-              Delete
+            <div className="card-action">
+              <div onClick={() => updateCard(id)} style={styles.margin} className="btn waves-effect">
+                Reveal Answer
+              </div>
+              <div className="btn red waves-effect" onClick={() => deleteCard(id)}>
+                Delete
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     )
   }
 }
